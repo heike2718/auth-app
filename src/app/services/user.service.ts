343ddd6e5f525188fabb3a3caac612ffe1345927
Heike Winkelvoß <public@egladil.de>
2019-01-05 08:11:58 +0100

    Rätsel des doppelten Aufrufs von createUser gelöst (sign-in)

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,30 +4,42 @@ import { RegistrationCredentials } from '../shared/model/registration-credential
 import { AuthErrorService } from './auth-error.service';
 import { environment } from '../../environments/environment';
 import { createHash } from '../shared/model/auth-response-data';
-import { map, publishLast, refCount, share } from 'rxjs/operators';
+import { map, publishLast, refCount, share, finalize, first, tap } from 'rxjs/operators';
 import { ResponsePayload } from '../shared/model/message';
 import { AppData } from '../shared/app-data.service';
+import { Logger } from '@nsalaun/ng-logger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: Http, private authErrorService: AuthErrorService, private appData: AppData) {}
+  constructor(private http: Http, private authErrorService: AuthErrorService, private appData: AppData, private logger: Logger) {}
 
   public registerUser(registrationCredentials: RegistrationCredentials): void {
 
+    this.logger.debug('registerUser: start');
+
+    // const headers = new Headers(); headers.append('Content-Type', 'application/json');
     const url = environment.apiUrl + '/users';
 
     // Bei Erfolg: ReponsePayload mit INFO-Message
     const redirectUrl = registrationCredentials.clientCredentials.redirectUrl;
+
     this.http.post(url, registrationCredentials).pipe(
       map(res => <ResponsePayload>res.json()),
-      share()
+      publishLast(),
+      refCount(),
+      tap(
+        () => this.logger.debug('inside pipe')
+      )
     ).subscribe(
-      payload => this.appData.updateRedirectUrl(redirectUrl + createHash(payload.data)),
-      error => this.authErrorService.handleError(error, 'registerUser')
-    )
+      payload => {
+        this.appData.updateRedirectUrl(redirectUrl + createHash(payload.data));
+      },
+      error => this.authErrorService.handleError(error, 'registerUser'),
+      () => this.logger.debug('post call completed')
+    );
   }
 }
 
