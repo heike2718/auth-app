81e7fe385b951fafb543006abb9abdc415485e70
Heike Winkelvoß <public@egladil.de>
2019-06-09 08:00:17 +0200

    small devices: less text smaller fonts released

--- src/environments/environment.qs.ts
+++ src/environments/environment.qs.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: '1.1.2',
+  version: '1.1.3',
   envName: 'Test',
   apiUrl: 'http://localhost:9000/authprovider',
   assetsUrl: 'auth-app/assets'
