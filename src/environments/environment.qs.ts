c057402e6450d66d8aaba23a556929ee0718dd25
Heike Winkelvoß <public@egladil.de>
2020-03-18 07:42:31 +0100

    update to angular 9 released

--- src/environments/environment.qs.ts
+++ src/environments/environment.qs.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 	production: false,
-	version: '3.4.0',
+	version: '4.0.0',
 	envName: 'Test',
 	apiUrl: 'http://localhost:9000/authprovider',
 	profilUrl: 'http://localhost:4200',
